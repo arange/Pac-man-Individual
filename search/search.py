2d93cc5fbc2a10dc24ab783cf1a1996a5c0e38e4
arange <panpan2558@gmail.com>
2018-08-26 17:49:23 +1000

    question 1 and 2 tested, not legalised

--- search/search.py
+++ search/search.py
@@ -20,131 +20,150 @@ Pacman agents (in searchAgents.py).
 import util
 
 class SearchProblem:
-    """
-    This class outlines the structure of a search problem, but doesn't implement
-    any of the methods (in object-oriented terminology: an abstract class).
+	"""
+	This class outlines the structure of a search problem, but doesn't implement
+	any of the methods (in object-oriented terminology: an abstract class).
 
-    You do not need to change anything in this class, ever.
-    """
+	You do not need to change anything in this class, ever.
+	"""
 
-    def getStartState(self):
-        """
-        Returns the start state for the search problem.
-        """
-        util.raiseNotDefined()
+	def getStartState(self):
+		"""
+		Returns the start state for the search problem.
+		"""
+		util.raiseNotDefined()
 
-    def isGoalState(self, state):
-        """
-          state: Search state
+	def isGoalState(self, state):
+		"""
+		  state: Search state
 
-        Returns True if and only if the state is a valid goal state.
-        """
-        util.raiseNotDefined()
+		Returns True if and only if the state is a valid goal state.
+		"""
+		util.raiseNotDefined()
 
-    def getSuccessors(self, state):
-        """
-          state: Search state
+	def getSuccessors(self, state):
+		"""
+		  state: Search state
 
-        For a given state, this should return a list of triples, (successor,
-        action, stepCost), where 'successor' is a successor to the current
-        state, 'action' is the action required to get there, and 'stepCost' is
-        the incremental cost of expanding to that successor.
-        """
-        util.raiseNotDefined()
+		For a given state, this should return a list of triples, (successor,
+		action, stepCost), where 'successor' is a successor to the current
+		state, 'action' is the action required to get there, and 'stepCost' is
+		the incremental cost of expanding to that successor.
+		"""
+		util.raiseNotDefined()
 
-    def getCostOfActions(self, actions):
-        """
-         actions: A list of actions to take
+	def getCostOfActions(self, actions):
+		"""
+		 actions: A list of actions to take
 
-        This method returns the total cost of a particular sequence of actions.
-        The sequence must be composed of legal moves.
-        """
-        util.raiseNotDefined()
+		This method returns the total cost of a particular sequence of actions.
+		The sequence must be composed of legal moves.
+		"""
+		util.raiseNotDefined()
 
 
 def tinyMazeSearch(problem):
-    """
-    Returns a sequence of moves that solves tinyMaze.  For any other maze, the
-    sequence of moves will be incorrect, so only use this for tinyMaze.
-    """
-    from game import Directions
-    s = Directions.SOUTH
-    w = Directions.WEST
-    return  [s, s, w, s, w, w, s, w]
+	"""
+	Returns a sequence of moves that solves tinyMaze.  For any other maze, the
+	sequence of moves will be incorrect, so only use this for tinyMaze.
+	"""
+	from game import Directions
+	s = Directions.SOUTH
+	w = Directions.WEST
+	return  [s, s, w, s, w, w, s, w]
+
+def blindSearch(problem,structure):
+	"*** YOUR CODE HERE ***"
+	
+
+	# print "Start:", problem.getStartState()
+	# print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+	# print "Start's successors:", problem.getSuccessors(problem.getStartState())
+	
+	# Store the path from start point to the current state
+	storage = structure
+	# example of state: (34, 16)
+	startState = problem.getStartState()
+	# example of successors: [((34, 15), 'South', 1), ((33, 16), 'West', 1)]
+	startNode = (startState,'Stop',0)
+	visitedList = []
+	storage.push([startNode])
+
+	while not storage.isEmpty():
+		"*** Using backtrack is more complicated ***"
+		# route.append(storage.pop())
+		route = storage.pop()
+		currentNode = route[-1]
+		currentState = currentNode[0]
+		"*** The successors can not be added here, otherwise the goal state will be expanded ***"
+		"*** This is due to the requirement of autograding ***"
+		# successors = problem.getSuccessors(currentState)
+
+		# check if current state is the goal state
+		if problem.isGoalState(currentState):
+			# return the correct route (directions) from 2nd node to goal 
+			return [x[1] for x in route][1:]
+		if currentState not in visitedList:
+			visitedList.append(currentState)
+			successors = problem.getSuccessors(currentState)
+			for successor in successors:
+				if successor[0] not in visitedList:
+					# storage.push(successor)
+					successorPath = route[:]
+					successorPath.append(successor)
+					storage.push(successorPath)
+
+	return False
+
 
 def depthFirstSearch(problem):
-    """
-    Search the deepest nodes in the search tree first.
+	"""
+	Search the deepest nodes in the search tree first.
 
-    Your search algorithm needs to return a list of actions that reaches the
-    goal. Make sure to implement a graph search algorithm.
+	Your search algorithm needs to return a list of actions that reaches the
+	goal. Make sure to implement a graph search algorithm.
 
-    To get started, you might want to try some of these simple commands to
-    understand the search problem that is being passed in:
+	To get started, you might want to try some of these simple commands to
+	understand the search problem that is being passed in:
 
-    print "Start:", problem.getStartState()
-    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
-    print "Start's successors:", problem.getSuccessors(problem.getStartState())
-    """
-    # Command you should run is:
-    # python pacman.py -l mediumMaze -p SearchAgent -a fn=dfs
-    "*** YOUR CODE HERE ***"
-    from util import Stack
+	print "Start:", problem.getStartState()
+	print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+	print "Start's successors:", problem.getSuccessors(problem.getStartState())
+	"""
+	# Command you should run is:
+	# python pacman.py -l mediumMaze -p SearchAgent -a fn=dfs
+	"*** YOUR CODE HERE ***"
+	structure = util.Stack()
 
-    print "Start:", problem.getStartState()
-    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
-    print "Start's successors:", problem.getSuccessors(problem.getStartState())
-    
-    storage = Stack()
-    path = []
-    # example of state: (34, 16)
-    startState = problem.getStartState()
-    # example of successors: [((34, 15), 'South', 1), ((33, 16), 'West', 1)]
-    startNode = (startState,'STOP',0)
-    visitedList = []
-    storage.push([startNode])
+	return blindSearch(problem,structure)
 
-    while not storage.isEmpty():
-    	# path.append(storage.pop())
-    	path = storage.pop()
-    	currentNode = path[-1]
-    	currentState = currentNode[0]
-    	successors = problem.getSuccessors(currentState)
-    	if problem.isGoalState(currentState):
-    			return [x[1] for x in path][1:]
-    	if currentState not in visitedList:
-    		visitedList.append(currentState)
-    		for successor in successors:
-    			if successor[0] not in visitedList:
-    				# storage.push(successor)
-    				successorPath = path[:]
-    				successorPath.append(successor)
-    				storage.push(successorPath)
-
-
-    util.raiseNotDefined()
+	# util.raiseNotDefined()
 
 def breadthFirstSearch(problem):
-    """Search the shallowest nodes in the search tree first."""
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+	"""Search the shallowest nodes in the search tree first."""
+	"*** YOUR CODE HERE ***"
+	structure = util.Queue()
+
+	return blindSearch(problem,structure)
+	
+	# util.raiseNotDefined()
 
 def uniformCostSearch(problem):
-    """Search the node of least total cost first."""
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+	"""Search the node of least total cost first."""
+	"*** YOUR CODE HERE ***"
+	util.raiseNotDefined()
 
 def nullHeuristic(state, problem=None):
-    """
-    A heuristic function estimates the cost from the current state to the nearest
-    goal in the provided SearchProblem.  This heuristic is trivial.
-    """
-    return 0
+	"""
+	A heuristic function estimates the cost from the current state to the nearest
+	goal in the provided SearchProblem.  This heuristic is trivial.
+	"""
+	return 0
 
 def aStarSearch(problem, heuristic=nullHeuristic):
-    """Search the node that has the lowest combined cost and heuristic first."""
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+	"""Search the node that has the lowest combined cost and heuristic first."""
+	"*** YOUR CODE HERE ***"
+	util.raiseNotDefined()
 
 
 # Abbreviations
